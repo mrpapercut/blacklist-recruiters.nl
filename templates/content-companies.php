d6caa70399c5a250184e72a093ae563066cf28d0
mrpapercut <mischa.rodermond@gmail.com>
2017-11-01 10:47:52 +0100

    Line-height

--- templates/content-companies.php
+++ templates/content-companies.php
@@ -7,8 +7,9 @@
 	<ul>
 	<?php
 	foreach ($content as $company) {
+        $em = '1.'.str_pad($company['reportcount'], 2, '0', STR_PAD_LEFT).'em';
 		echo '<li data-id="'.$company['id'].'">
-            <a href="bedrijven/'.$company['id'].'" style="font-size:1.'.str_pad($company['reportcount'], 2, '0', STR_PAD_LEFT).'em">
+            <a href="bedrijven/'.$company['id'].'" style="font-size:'.$em.';line-height:'.$em.'">
             '.ucwords($company['companyname']).' ('.$company['reportcount'].')
             </a>
         </li>'."\n";
